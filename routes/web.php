2c09a3f01239032d13340923756173e122bbd297
doctorzeee <dimasrafif11@gmail.com>
2025-06-09 16:44:07 +0700

    Membuat Tampilan

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\Admin\DashboardController;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', [PageController::class, 'index'])->name('page.index');
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
+Route::get('/{any}', function(){
+    return view('notFoundPage');
+})->where('any', '.*');
